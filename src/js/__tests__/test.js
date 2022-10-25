6e5911d070191bbf39fbb42888c897f49f9d3115
Dmitriy <aglerimov05@mail.ru>
2022-10-25 19:36:11 +0300

    правки по замечаниям преподавателя

--- src/js/__tests__/test.js
+++ src/js/__tests__/test.js
@@ -4,11 +4,41 @@ const referenceValidname = true;
 const referenceInvalidname = false;
 
 test('validnametest', () => {
-    const nameuserValid = new Validator().validateUsername('dmitriy_30-05-67_agler');
-    expect(referenceValidname).toBe(nameuserValid);
-  });
+  const nameuserValid = new Validator().validateUsername('dmitriy_30-05-67_agler');
+  expect(referenceValidname).toBe(nameuserValid);
+});
 
 test('invalidnametest', () => {
-    const nameuserInValid = new Validator().validateUsername('dmitriy_30-05-67_agler11');
-    expect(referenceInvalidname).toBe(nameuserInValid);
+  const nameuserInValid = new Validator().validateUsername('dmitriy_30-05-67_agler11');
+  expect(referenceInvalidname).toBe(nameuserInValid);
+});
+
+test('invalidnametest', () => {
+  const nameuserInValid_1 = new Validator().validateUsername('dmitriy_30-05-67_agler_');
+  expect(referenceInvalidname).toBe(nameuserInValid_1);
+});
+
+test('invalidnametest', () => {
+  const nameuserInValid_2 = new Validator().validateUsername('dmitriy_30-05-67_agler-');
+  expect(referenceInvalidname).toBe(nameuserInValid_2);
+});
+
+test('invalidnametest', () => {
+  const nameuserInValid_3 = new Validator().validateUsername('dmitriy_30-05-1967_agler');
+  expect(referenceInvalidname).toBe(nameuserInValid_3);
+});
+
+test('invalidnametest', () => {
+  const nameuserInValid_4 = new Validator().validateUsername('1dmitriy_30-05-67_agler');
+  expect(referenceInvalidname).toBe(nameuserInValid_4);
+});
+
+test('invalidnametest', () => {
+  const nameuserInValid_5 = new Validator().validateUsername('_dmitriy_30-05-67_agler');
+  expect(referenceInvalidname).toBe(nameuserInValid_5);
+});
+
+test('invalidnametest', () => {
+  const nameuserInValid_6 = new Validator().validateUsername('-dmitriy_30-05-67_agler');
+  expect(referenceInvalidname).toBe(nameuserInValid_6);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,13 @@
-export default class Validator{
+export default class Validator {
   validateUsername(username) {
-      let regexp = /^[^\d_\-]+(\D|\d(\D|$)|\d\d(\D|$))+[^\d_\-]+$/;
-      let forbiddenSymbols = username.match(regexp);
-      if (forbiddenSymbols === null) {
-        console.log('Ваше имя не соответствует требованиям. Измените имя');
-        return false
-      } else console.log('Ваше имя соответствует требованиям.'); 
-      return true;
+    let regexp_1 = /^[a-zA-Z][a-zA-Z0-9\-\_]+[a-zA-Z]$/;
+    let regexp_2 = /[0-9]{4,}/;
+    let forbiddenSymbols_1 = username.match(regexp_1);
+    let forbiddenSymbols_2 = username.search(regexp_2);
+    if (forbiddenSymbols_1 === null || forbiddenSymbols_2 != -1) {
+      console.log('Ваше имя не соответствует требованиям. Измените имя');
+      return false
+    } else console.log('Ваше имя соответствует требованиям.');
+    return true;
   }
 }
